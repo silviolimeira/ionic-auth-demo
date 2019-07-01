af8e5fe32e148712eb20a277857657ec04dc9e32
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-07-01 07:43:46 -0300

    setting up forms

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, HttpClientModule]
+  imports: [CommonModule, HttpClientModule, FormsModule]
 })
 export class AuthModule {}
