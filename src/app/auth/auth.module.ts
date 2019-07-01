5514b1435bd76f5c6e14ef1169794a8b19bd813e
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-07-01 07:51:17 -0300

    Setting up Ionic Storage Module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from "@angular/common";
 
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
+import { IonicStorageModule } from "@ionic/storage";
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, HttpClientModule, FormsModule]
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    IonicStorageModule.forRoot()
+  ]
 })
 export class AuthModule {}
